2dae967810511541801691a9e9583197f08be2f5
Ralstonnn <zigthehedge2002@gmail.com>
2023-09-01 02:41:13 +0500

    CI/CD test

--- src/components/views/MainView.tsx
+++ src/components/views/MainView.tsx
@@ -8,6 +8,7 @@ export default function MainView() {
     <Container>
       <Header />
       <div className="main-view">
+        asfdasfasfasfasfasdf
         <Outlet />
       </div>
     </Container>
